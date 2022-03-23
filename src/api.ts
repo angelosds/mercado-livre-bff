12eb6dc453d75e5e2fdea9dc2db62c687ae2e3af
Angelo Santos <angelosds@gmail.com>
2022-03-23 13:02:01 -0300

    Removed unused controllers

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express';
 
-import healthCheck from '@components/healthcheck/healthCheck.router';
-import user from '@components/user/user.router';
 import items from '@components/items/items.router';
 
 const router: Router = Router();
-router.use(healthCheck);
-router.use(user);
 router.use(items);
 
 export default router;
